66c3c34a697ff48befbbc1f174b4d6ae21b00320
PaulW02 <pslwi@ug.kth.se>
2022-11-29 14:34:34 +0100

    First commit

---
+++ src/main/java/com/example/database_lab1/model/SearchMode.java
@@ -0,0 +1,5 @@
+package com.example.database_lab1.model;
+
+public enum SearchMode {
+    Title, ISBN, Author
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module se.kth.anderslm.booksdb {
+    requires javafx.controls;
+    requires javafx.base;
+
+    opens com.example.database_lab1 to javafx.base;
+    opens com.example.database_lab1.model to javafx.base; // open model package for reflection from PropertyValuesFactory (sigh ...)
+    exports com.example.database_lab1;
+
+    requires java.sql;
+}
